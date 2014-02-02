7a93f1dcb67b2b9dde5c7c2443674ae27a644c67
bhawkeswood <bhawkeswood@gmail.com>
2014-02-02 12:27:52 -0500

    Began setting up user sign up controller view and form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to root_url, :notice => "You're all signed up!  Welcome to UF!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
